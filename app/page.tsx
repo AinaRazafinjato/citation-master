7ee6dfc34f2e2ba86d72adaee52a0e630e820be2
Parlss29 <AinaRazafinjato@users.noreply.github.com>
2025-07-29 20:04:09 +0300

    feat(ui): add shadcn ui on the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import {Button} from '../src/components/ui/button';
+import {Input} from '../src/components/ui/input';
+
 export default function Home() {
   return (
-    <div className="text-center">Bonsoir !</div>
+    <div className="item-center">
+      <Button>Bonsoir</Button>
+      <Input />
+    </div>
   );
 }
