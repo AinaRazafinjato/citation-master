3a833959ebbd9ab4ee78d3d5a50ce54d4115b1c7
Parlss29 <AinaRazafinjato@users.noreply.github.com>
2025-07-29 20:36:21 +0300

    feat: update global styles and add admin page component

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+import {Card, CardHeader, CardTitle} from '../../src/components/ui/card';
+
+export default function Admin() {
+    return(
+        <Card>
+            <CardHeader>
+                <CardTitle>URL: /admin</CardTitle>
+            </CardHeader>
+        </Card>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import {Button} from '../src/components/ui/button';
-import {Input} from '../src/components/ui/input';
+import {Card, CardHeader, CardTitle} from '../src/components/ui/card';
 
 export default function Home() {
   return (
     <div className="item-center">
-      <Button>Bonsoir</Button>
-      <Input />
+      <Card className="max-w-sm mx-auto mt-10">
+        <CardHeader>
+          <CardTitle>Welcome to the App</CardTitle>
+          <p>This is a simple application demonstrating the use of components.</p>
+          <Button>Get Started</Button>
+          <Button className='bg-white text-black-500 hover:bg-gray-200 border'>Learn More</Button>
+        </CardHeader>
+      </Card>
     </div>
   );
 }
